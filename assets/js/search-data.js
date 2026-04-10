7f73025e68cb2bb5fddefcbee459c6315c5ee98c
sgraine <50187455+sgraine@users.noreply.github.com>
2026-04-10 06:23:16 +0000

    Deploying to gh-pages from @ sgraine/sgraine.github.io@b0fb6f43b16b1cd5c3a008caa84f2c2d7f132248 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,16 +412,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-awarded-the-atse-technology-scholarship-to-attend-science-meets-parliament-2026",
+          title: 'Awarded the ATSE Technology Scholarship to attend Science Meets Parliament 2026!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
