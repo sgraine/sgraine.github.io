707be6c9d39eadc7be8cec1376148e3cecb30d71
sgraine <50187455+sgraine@users.noreply.github.com>
2026-04-28 05:55:25 +0000

    Deploying to gh-pages from @ sgraine/sgraine.github.io@e09206ba2e0d22c4e5193c3e1fcff943da849176 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-supervision",
+          title: "Supervision",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/supervision/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
