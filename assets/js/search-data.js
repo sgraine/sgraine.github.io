bb2669de803bc07261393a9253173515c870c731
sgraine <50187455+sgraine@users.noreply.github.com>
2026-04-29 05:12:48 +0000

    Deploying to gh-pages from @ sgraine/sgraine.github.io@37697bfd08e5e2f402fc689970a6c268bf93f06e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,7 +42,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/https:/github.com/sgraine/marine-robotics-seminars";
+            window.location.href = "/https:/www.scarlettraine.com/marine-robotics-seminars/";
           },
         },{id: "nav-supervision",
           title: "Supervision",
